059d9da8e70e7274252675236d0b6d9304a83785
pierzbarry <pmbarry@asu.edu>
2019-10-21 16:16:02 -0400

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // printing out hello world to the console
 	System.out.println("hey loser");
+
+	    // declaration of a variable
+    String name;
+
+        // inititalization
+    name = "pierz";
+
+        // declaration and inititalization of a variable in the same line
+    String newName = "helloKitty";
+
+        //reinitialization of a variable
+        System.out.print(name);
     }
 }
